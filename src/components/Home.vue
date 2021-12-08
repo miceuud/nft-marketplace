e68e4e1dc5f626fe0141313d2dcb12a5028ed6dc
miceuud <miceuud@.gmail.com>
2021-12-08 13:50:22 +0100

    vue setup

---
+++ src/components/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="">
+    <ul v-for="link in links" :key="link.title">
+      <li></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  props: {},
+  data() {
+    return {
+      header: "UMT NFT Marketplace",
+      links: [
+        {
+          title: "Collections",
+          url: "",
+        },
+        {
+          title: "My assets",
+          url: "",
+        },
+        {
+          title: "Dashbaord",
+          url: "",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
