1fddca95203140138ccf8756ef2ee88a50889a1f
miceuud <miceuud@.gmail.com>
2021-12-09 19:49:37 +0100

    Adding VueX

---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    account: "",
+    assets: [],
+  },
+  mutations: {
+    CREATE_ASSETS(state,value) {
+      state.assets.push(value)
+    },
+  },
+  actions: {},
+  modules: {},
+});
