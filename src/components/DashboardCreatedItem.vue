63a06019706bcf2ef7cab8ab2fa6e3e0260f825f
miceuud <miceuud@.gmail.com>
2021-12-09 13:16:37 +0100

    Frontend views and component

---
+++ src/components/DashboardCreatedItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <b-container>
+      <b-row>
+        <b-card-group>
+          <b-col lg="4">
+            <b-card
+              title="Title"
+              img-src="https://placekitten.com/g/380/200"
+              img-alt="Image"
+              img-top
+            >
+              <b-card-text>
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </b-card-text>
+              <template #footer>
+                <small class="text-muted">Last updated 3 mins ago</small>
+                <b-button href="#" variant="primary">Buy</b-button>
+              </template>
+            </b-card>
+          </b-col>
+        </b-card-group>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+// import {} from "";
+export default {
+  data() {
+    return {
+      createdItem: [],
+    };
+  },
+  components: {},
+};
+</script>
+
+<style></style>
---
+++ src/views/CreateNft.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <!-- receive props -->
+    <create-assets :asset="asset" @createdAssets="createdAssets" />
+  </div>
+</template>
+
+<script>
+import CreateAssets from "../components/CreateAssetsForm.vue";
+export default {
+  data() {
+    return {
+      // receive props
+      asset: {
+        ame: "",
+        description: "",
+        amount: "",
+        file: null,
+      },
+    };
+  },
+  components: {
+    CreateAssets,
+  },
+  methods: {
+    // receive from child component
+    createdAssets() {
+      let name = this.asset.name;
+
+      let description = this.asset.description;
+      let amount = this.asset.amount;
+
+      console.log(name, description, amount);
+    },
+  },
+};
+</script>
+
+<style></style>
